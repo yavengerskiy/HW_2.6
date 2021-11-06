0a8300d10e20e857732b56e54f3b8c91a05e815a
YaYuVengerskiy <yavengerskiy@yandex.ru>
2021-11-06 23:22:35 +0500

    only storyboard

--- HW_2.6/ViewController.swift
+++ HW_2.6/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var previewColor: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    
+    @IBOutlet weak var redInputText: UITextField!
+    @IBOutlet weak var greenInputText: UITextField!
+    @IBOutlet weak var blueInputText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        previewColor.layer.cornerRadius = 10
+        
     }
 
 
